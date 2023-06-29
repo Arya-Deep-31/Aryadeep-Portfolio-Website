56ac94cece57026e2e8b217a7e02b9d61ee8fad4
Aryadeep Gogoi <100404453+Alsi-Aryadeep31@users.noreply.github.com>
2023-06-30 04:34:42 +0530

    UPDATING HOME PAGE

---
+++ src/components/AnimatedText.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const AnimatedText = ({text , className = " "}) => {
+  return (
+    <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden'>
+      <h1 className={`inline-block w-full  text-dark font-semibold text-5xl flex items-center justify-center h-screen relative ${className} `} >
+        {
+          text.split(" ").map((word,index) => 
+            <span key={word+'-'+index}>
+              {word}&nbsp;
+            </span>
+          )
+          
+        }</h1>
+      
+    </div>
+  )
+}
+
+export default AnimatedText
