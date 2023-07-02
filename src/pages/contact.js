08df508efb4dd06f9c887287c9a7a42754779111
Aryadeep Gogoi <100404453+Alsi-Aryadeep31@users.noreply.github.com>
2023-07-02 22:56:20 +0530

    UPDATING CONTACT PAGE

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -57,7 +57,7 @@ const contact = () => {
             setTouched({});
             setState(initState);
             toast({
-                title: "Message sent.",
+                title: "Message sent to Aryadeep Gogoi.",
                 status: "success",
                 duration: 2000,
                 position: "top",
@@ -83,10 +83,10 @@ const contact = () => {
             </Head>
 
             <main>
-                <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 2 }}>
+                <motion.div className='' initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 2 }}>
                     <Layout className=''>
                         <AnimatedText text="Connect With Me" className=' ' />
-                        <AnimatedText text="Or drop a feedback" className=' !text-3xl font-bold text-dark/70' />
+                        <AnimatedText text="Or drop a feedback" className=' !text-3xl font-bold text-dark/70 mb-10' />
                     </Layout>
 
                     {error && (
@@ -95,78 +95,84 @@ const contact = () => {
                         </Text>
                     )}
 
-                    <FormControl isRequired isInvalid={touched.name && !values.name} mb={5}>
-                        <FormLabel>Name</FormLabel>
-                        <Input
-                            type="text"
-                            name="name"
-                            errorBorderColor="red.300"
-                            value={values.name}
-                            onChange={handleChange}
-                            onBlur={onBlur}
-                        />
-                        <FormErrorMessage>Required</FormErrorMessage>
-                    </FormControl>
+                    <div className='grid col-12'>
 
-                    <FormControl isRequired isInvalid={touched.email && !values.email} mb={5}>
-                        <FormLabel>Email</FormLabel>
-                        <Input
-                            type="email"
-                            name="email"
-                            errorBorderColor="red.300"
-                            value={values.email}
-                            onChange={handleChange}
-                            onBlur={onBlur}
-                        />
-                        <FormErrorMessage>Required</FormErrorMessage>
-                    </FormControl>
+                        
 
-                    <FormControl
-                        mb={5}
-                        isRequired
-                        isInvalid={touched.subject && !values.subject}
-                    >
-                        <FormLabel>Subject</FormLabel>
-                        <Input
-                            type="text"
-                            name="subject"
-                            errorBorderColor="red.300"
-                            value={values.subject}
-                            onChange={handleChange}
-                            onBlur={onBlur}
-                        />
-                        <FormErrorMessage>Required</FormErrorMessage>
-                    </FormControl>
+                        <FormControl className='grid w-full items-center justify-center' isRequired isInvalid={touched.name && !values.name} mb={5}>
+                            <FormLabel className='text-dark/80 font-bold'>Enter Your Name</FormLabel>
+                            <Input className='rounded-full w-full mb-2'
+                                type="text"
+                                name="name"
+                                errorBorderColor="red.300"
+                                value={values.name}
+                                onChange={handleChange}
+                                onBlur={onBlur}
+                            />
+                            <FormErrorMessage>Required</FormErrorMessage>
+                        </FormControl>
 
-                    <FormControl
-                        isRequired
-                        isInvalid={touched.message && !values.message}
-                        mb={5}
-                    >
-                        <FormLabel>Message</FormLabel>
-                        <Textarea
-                            type="text"
-                            name="message"
-                            rows={4}
-                            errorBorderColor="red.300"
-                            value={values.message}
-                            onChange={handleChange}
-                            onBlur={onBlur}
-                        />
-                        <FormErrorMessage>Required</FormErrorMessage>
-                    </FormControl>
+                        <FormControl className='grid w-full items-center justify-center' isRequired isInvalid={touched.email && !values.email} mb={5}>
+                            <FormLabel className='text-dark/80 font-bold'>Enter Your Email</FormLabel>
+                            <Input  className='rounded-full w-full mb-2'
+                                type="email"
+                                name="email"
+                                errorBorderColor="red.300"
+                                value={values.email}
+                                onChange={handleChange}
+                                onBlur={onBlur}
+                            />
+                            <FormErrorMessage>Required</FormErrorMessage>
+                        </FormControl>
 
-                    <Button
-                        variant="outline"
-                        colorScheme="blue"
-                        isLoading={isLoading}
-                        disabled={
-                            !values.name || !values.email || !values.subject || !values.message
-                        }
-                        onClick={onSubmit}
-                    >
-                        Submit
-                    </Button>
+                        <FormControl className='grid w-full items-center justify-center'
+                            mb={5}
+                            isRequired
+                            isInvalid={touched.subject && !values.subject}
+                        >
+                            <FormLabel className='text-dark/80 font-bold'>Subject</FormLabel>
+                            <Input  className='rounded-full w-full mb-2'
+                                type="text"
+                                name="subject"
+                                errorBorderColor="red.300"
+                                value={values.subject}
+                                onChange={handleChange}
+                                onBlur={onBlur}
+                            />
+                            <FormErrorMessage>Required</FormErrorMessage>
+                        </FormControl>
+
+                        <FormControl className='grid w-full items-center justify-center'
+                            isRequired
+                            isInvalid={touched.message && !values.message}
+                            mb={5}
+                        >
+                            <FormLabel  className='text-dark/80 font-bold'>Message</FormLabel>
+                            <Textarea  className='rounded-25% w-full mb-2'
+                                type="text"
+                                name="message"
+                                rows={4}
+                                errorBorderColor="red.300"
+                                value={values.message}
+                                onChange={handleChange}
+                                onBlur={onBlur}
+                            />
+                            <FormErrorMessage>Required</FormErrorMessage>
+                        </FormControl>
+
+                        <Button
+                            variant="outline"
+                            colorScheme="blue"
+                            isLoading={isLoading}
+                            disabled={
+                                !values.name || !values.email || !values.subject || !values.message
+                            }
+                            onClick={onSubmit}
+                        >
+                            Submit
+                        </Button>
+
+                    </div>
 
                 </motion.div>
 
