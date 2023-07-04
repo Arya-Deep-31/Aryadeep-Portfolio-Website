ff3a0c610a566f047fe8b78153ee81725f09c329
Aryadeep Gogoi <100404453+Alsi-Aryadeep31@users.noreply.github.com>
2023-07-05 02:52:33 +0530

    updates

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,18 +3,18 @@ import Head from 'next/head'
 import AnimatedText from '@/components/AnimatedText'
 import Layout from '@/components/Layout'
 import { motion } from "framer-motion";
-// import {
-//     Button,
-//     Container,
-//     FormControl,
-//     FormErrorMessage,
-//     FormLabel,
-//     Heading,
-//     Input,
-//     Text,
-//     Textarea,
-//     useToast,
-// } from "@chakra-ui/react";
+import {
+    Button,
+    Container,
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    Heading,
+    Input,
+    Text,
+    Textarea,
+    useToast,
+} from "@chakra-ui/react";
 import { sendContactForm } from "../lib/api";
 import Map from "../components/map"
 import TransitionEffect from '@/components/TransitionEffect';
@@ -31,50 +31,50 @@ const contact = () => {
 
 
 
-    // const toast = useToast();
-    // const [state, setState] = useState(initState);
-    // const [touched, setTouched] = useState({});
+    const toast = useToast();
+    const [state, setState] = useState(initState);
+    const [touched, setTouched] = useState({});
 
-    // const { values, isLoading, error } = state;
+    const { values, isLoading, error } = state;
 
-    // const onBlur = ({ target }) =>
-    //     setTouched((prev) => ({ ...prev, [target.name]: true }));
+    const onBlur = ({ target }) =>
+        setTouched((prev) => ({ ...prev, [target.name]: true }));
 
-    // const handleChange = ({ target }) =>
-    //     setState((prev) => ({
-    //         ...prev,
-    //         values: {
-    //             ...prev.values,
-    //             [target.name]: target.value,
-    //         },
-    //     }));
+    const handleChange = ({ target }) =>
+        setState((prev) => ({
+            ...prev,
+            values: {
+                ...prev.values,
+                [target.name]: target.value,
+            },
+        }));
 
 
 
-    // const onSubmit = async () => {
-    //     setState((prev) => ({
-    //         ...prev,
-    //         isLoading: true,
-    //     }));
-    //     try {
-    //         await sendContactForm(values);
-    //         setTouched({});
-    //         setState(initState);
-    //         toast({
-    //             title: "Message sent to Aryadeep Gogoi",
-    //             status: "success",
-    //             duration: 2000,
-    //             position: "top",
+    const onSubmit = async () => {
+        setState((prev) => ({
+            ...prev,
+            isLoading: true,
+        }));
+        try {
+            await sendContactForm(values);
+            setTouched({});
+            setState(initState);
+            toast({
+                title: "Message sent to Aryadeep Gogoi",
+                status: "success",
+                duration: 2000,
+                position: "top",
                 
-    //         });
-    //     } catch (error) {
-    //         setState((prev) => ({
-    //             ...prev,
-    //             isLoading: false,
-    //             error: error.message,
-    //         }));
-    //     }
-    // };
+            });
+        } catch (error) {
+            setState((prev) => ({
+                ...prev,
+                isLoading: false,
+                error: error.message,
+            }));
+        }
+    };
 
 
 
@@ -98,7 +98,7 @@ const contact = () => {
 
 
 
-                    {/* <div className='  dark:bg-darker/80 dark:shadow-primary/60 dark:border-primary p-10 mx-auto flex flex-col items-center justify-center rounded-3xl border border-solid  bg-white/40 shadow-2xl w-full  mb-20'>
+                    <div className='  dark:bg-darker/80 dark:shadow-primary/60 dark:border-primary p-10 mx-auto flex flex-col items-center justify-center rounded-3xl border border-solid  bg-white/40 shadow-2xl w-full  mb-20'>
 
                         {error && (
                             <Text className='dark:text-light text-dark text-xl mb-2' >
@@ -195,7 +195,7 @@ const contact = () => {
 
 
 
-                    </div> */}
+                    </div>
 
 
 
